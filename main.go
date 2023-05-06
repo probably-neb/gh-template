e3e49eef91a39a37c2dc69304b8c3928652cf4fb
Ben Kunkle <ben.kunkle@gmail.com>
2023-05-06 11:57:05 -0700

    retrieving list of .gitignore files

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/cli/go-gh/v2/pkg/api"
 )
+
+type GitFile struct {
+	Type         string
+	Name         string
+	Download_Url string
+}
+
+func getGitignoreEntries(client *api.RESTClient) ([]GitFile,error) {
+    response := []GitFile{}
+    err := client.Get("repos/github/gitignore/contents/", &response)
+	if err != nil {
+		return nil, err
+	}
+    // TODO: get gitignores from community and global directories
+    return response, nil
+}
 
 func main() {
 	fmt.Println("hi world, this is the gh-template extension!")
@@ -13,13 +28,14 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
+    files, err := getGitignoreEntries(client)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("running as %s\n", response.Login)
+	for _, file := range files {
+		fmt.Printf("%+v\n", file)
+	}
 }
 
 // For more examples of using go-gh, see:
